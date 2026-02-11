07d5504f85e8c94b73c956a4dae97e832a975826
Rodion <kurochkinr.06@gmail.com>
2026-02-11 09:48:53 +0300

    Translated into Russian

--- example/array.py
+++ example/array.py
@@ -1,13 +1,13 @@
 arr = list()
 n = int(input("Введите количество элементов: "))
-print("Введите элемент:")
+print("Введите элемент: ")
 i = 0
 while (i < n):
     tmp = int(input(""))
     arr.append(tmp)
     i += 1
 
-print("Список:")
+print("Список: ")
 i = 0
 while (i < n):
     print(arr[i], end = " ")
