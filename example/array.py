b66f0a2aac5240ea1a6b7205a0f244a8f29d1775
Rodion <kurochkinr.06@gmail.com>
2026-02-11 12:24:48 +0300

    Fix possible array overflow.

--- example/array.py
+++ example/array.py
@@ -1,28 +1,20 @@
-def input_list():
-    print("Введите элемент: ")
-    i = 0
-    while (i < n):
-        tmp = int(input(""))
-        arr.append(tmp)
-        i += 1
-
-
-def output_list():
-    print("Список: ")
-    i = 0
-    while (i < n):
-        print(arr[i], end=" ")
-        i += 1
-
 arr = list()
 i = True
-n = int(input("Введите количество элементов: "))
-
-
-
-if n > 15:
-    print("Максимум 15 элементов")
-    exit()
-input_list()
-output_list()
+while i:
+    n = int(input("Введите количество элементов: "))
+    if n <= 15:
+        i = False
+    else:
+        print("Максимум 15 элементов")
+print("Введите элемент: ")
+i = 0
+while (i < n):
+    tmp = int(input(""))
+    arr.append(tmp)
+    i += 1
+print("Список:")
+i = 0
+while (i < n):
+    print(arr[i], end = " ")
+    i += 1
 print("")
