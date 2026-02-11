253b3b6c2dcd9a1ca60500b8471d9cc74a601d2e
Katya <katyasivolgina@mail.ru>
2026-02-11 10:50:49 +0300

    IO functions were added.

--- example/array.py
+++ example/array.py
@@ -1,15 +1,21 @@
+def input_list():
+    print("Введите элемент: ")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+
+
+def output_list():
+    print("Список: ")
+    i = 0
+    while (i < n):
+        print(arr[i], end=" ")
+        i += 1
+
 arr = list()
 n = int(input("Введите количество элементов: "))
-print("Введите элемент: ")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
-
-print("Список: ")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
+input_list()
+output_list()
 print("")
